774fa64f1a58791a35bbbecde1e64f23100bd6ac
zia <letscode786@gmail.com>
2021-08-10 19:54:53 +0500

    updated homepage path

--- server.js
+++ server.js
@@ -372,9 +372,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
     
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+  app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+});
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
