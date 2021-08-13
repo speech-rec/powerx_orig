73aa604bb008330a1989fbe438d80d49f33064d2
zia <letscode786@gmail.com>
2021-08-13 19:04:48 +0500

    ..

--- server.js
+++ server.js
@@ -367,14 +367,22 @@ var url = `${baseURL}/api/api.ashx?methodname=GetTemplatesByUserId&userId=${user
   );
   
 });
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'build')));
+// if (process.env.NODE_ENV === 'production') {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, 'build')));
     
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+//   // Handle React routing, return all requests to React app
+//   app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
+// }
+
+if(process.env.NODE_ENV === 'production'){
+  const path  =  require('path');
+  app.get('/*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'build','index.html'))
+  })
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+var server = app.listen(port, () => console.log(`Listening on port ${port}`));
+server.timeout = 60000;
